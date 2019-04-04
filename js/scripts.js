c46198cb79707ed8e76c6277bf15acfff951c0e2
Chandrika Guntur <cguntur@gmail.com>
2019-04-04 05:35:55 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 jQuery(document).ready(function($){
   $('#wpcd_child_cat_loader, #child_cat_default_text, #taxonomy').css("display", "none");
   $("#wpcd_parent").change(function(){
-    //alert($(this).val());
     var parent_cat = $(this).val();
-    //alert(parent_cat);
     var child_cat_default_text = $("#child_cat_default_text").text();
-    //alert(child_cat_default_text);
     var taxonomy = $("#taxonomy").text();
-    //alert(taxonomy);
     $.ajax({
       url: wpcdajax.ajaxurl,
       type:'GET',
@@ -24,9 +20,7 @@ jQuery(document).ready(function($){
          $("#wpcd_child_cat_loader").hide();
       },
       success: function(response){
-        //alert(response);
         $("#child_cat_dropdown").html(response);
-        //$(this).siblings(".child_cat_dropdown").html("Test");
         $("#child_cat_dropdown").find("script").each(function(i) {
             eval($(this).text());
         });
