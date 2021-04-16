16e9be84a8f79cc97678957aefccbc760c26318d
Chandrika Guntur <cguntur@gcsdesign.com>
2021-04-15 20:01:32 -0700

    Multiple widgets are now working on the same page

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 jQuery(document).ready(function($){
-  $('#wpcd_child_cat_loader, #child_cat_default_text, #taxonomy, #exclude, #include').css("display", "none");
-  $("#wpcd_parent").change(function(){
+  $('#wpcd_child_cat_loader, #child_cat_default_text, #taxonomy, #exclude, #include, #random_id').css("display", "none");
+  $("select[name=wpcd_parent]").change(function(){
     var parent_cat = $(this).val();
+    $(this).parent().parent().addClass("Test");
+    var random_class = $(this).parent().parent().children("#random_id").text();
     var child_cat_default_text = $("#child_cat_default_text").text();
-    var taxonomy = $("#taxonomy").text();
+    var taxonomy = $(this).parent().parent().children("#taxonomy").text();
     var child_cats_exclude = $("#exclude").text();
     var child_cats_include = $("#include").text();
+    var child_cat_dropdown_selector = "#child_cat_dropdown"+"."+random_class;
     $.ajax({
       url: wpcdajax.ajaxurl,
       type:'GET',
@@ -24,8 +27,8 @@ jQuery(document).ready(function($){
          $("#wpcd_child_cat_loader").hide();
       },
       success: function(response){
-        $("#child_cat_dropdown").html(response);
-        $("#child_cat_dropdown").find("script").each(function(i) {
+        $(child_cat_dropdown_selector).html(response);
+        $(child_cat_dropdown_selector).find("script").each(function(i) {
             eval($(this).text());
         });
       },
